4994c42ca66d81a5de5a444e5a1519f9a24c72cc
Ujjwal Tiku <92386685+ujjwal-tiku@users.noreply.github.com>
2023-02-14 17:55:00 +0530

    commit change for ngrok

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Hello, Jenkins(PES2UG20CS475)\n";
+  cout << "I have successfully built and run using CONTINUOUS INTEGRATION\n";
   return 0;
 }
